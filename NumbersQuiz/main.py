468bfa8aecd3ad9bcaeea8ceba4de430ef10f7b3
Kevin Martinez <kevinadairmtzyanez@gmail.com>
2026-09-22 09:29:27 -0600

    Actualizacion API, en NumbersQuiz

--- NumbersQuiz/main.py
+++ NumbersQuiz/main.py
@@ -1,21 +1,26 @@
+import requests
+
 def trivia_fetch(num):
+
+  url = f"https://opentdb.com/api.php?amount={num}"
+  """
   trivia ={
     1: "La Tierra es el único planeta del Sistema Solar que posee una sola luna",
     2: "En el mundo hay por lo menos 2 personas identicas, tú y tu Doppelganger",
     45: "El significado de la vida, del universo y todo lo demas...",
     1000: "El final de todo...."
   }
-  texto = trivia.get(num,"Dato no creado....")
+  """
+  response = requests.get(url)
+  trivia = response.json()
   
-  return {"number":num,"text":texto}
+  return trivia
 
 def main():
-  print("Elige un numero para saber un dato random (1, 2, 42 o 1000)")
-  dato = int(input("-- Opcion: "))
-  texto = trivia_fetch(dato)
-  print(texto)
+  cantidad=int(input("Elige, cuantas preguntas quieres obtener\n --Opcion: "))
 
-
+  trivia = trivia_fetch(cantidad)
+  print(trivia)
 
 if __name__=="__main__":
   main()
--- NumbersQuiz/test.py
+++ NumbersQuiz/test.py
@@ -1,9 +1,11 @@
 from main import trivia_fetch
+import time
 
 # Test 1
 def test_trivia_42():
-  assert trivia_fetch(42)["number"] == 42
+  assert len(trivia_fetch(5)["results"]) == 5
 
 # Test 2
 def test_trivia_1000():
-  assert trivia_fetch(1000)["number"] == 1000
+  time.sleep(5)
+  assert trivia_fetch(5)["response_code"] == 0
